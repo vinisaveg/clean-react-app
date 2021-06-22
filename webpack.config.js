f9feab62404ef6ecbe7c7e83cedf1441bc90b7c2
vinisaveg <viniisaveeg@gmail.com>
2021-06-22 20:58:10 -0300

    chore: config scss

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,33 @@ module.exports = {
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
+        extensions: ['.ts', '.tsx', '.js', 'scss'],
         alias: {
             '@': path.join(__dirname, 'src'),
         }
+    },
+    module:  {
+        rules: [{
+            test: /\.ts(x?)$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/
+        },
+        {
+            test: /\.scss$/,
+            use: [{
+                loader: 'style-loader',
+            },
+            {
+                loader: 'css-loader',
+                options: {
+                    module: true
+                }
+            },
+            {
+                loader: 'sass-loader',
+            }],
+            exclude: /node_modules/
+        }]
     },
     devServer: {
         contentBase: './public',
