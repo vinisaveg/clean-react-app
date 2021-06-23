c48b950e4560a1205fd12b0bb49a80b6c40ea825
vinisaveg <viniisaveeg@gmail.com>
2021-06-23 20:10:29 -0300

    feat: login page init

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,4 +1,6 @@
+import Login from '@/presentation/pages/login'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<div>App init!</div>, document.getElementById('main'))
+ReactDOM.render(<Login />, document.getElementById('main'))
