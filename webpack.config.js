2bcc52cd4a3f6b9d308db27cc591fb434ddc9e87
vinisaveg <viniisaveeg@gmail.com>
2021-06-23 20:10:10 -0300

    fix: adjusting webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
             {
                 loader: 'css-loader',
                 options: {
-                    module: true
+                    modules: true
                 }
             },
             {
