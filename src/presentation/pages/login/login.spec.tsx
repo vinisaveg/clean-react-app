85664f1e8062711278102fd0434015caf4fa199c
vinisaveg <viniisaveeg@gmail.com>
2021-06-24 20:31:16 -0300

    test: ensure login start with input with requred state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -8,11 +8,17 @@ describe('Login component', () => {
     const { getByTestId } = render(<Login />)
 
     const errorWrap = getByTestId('errorWrap')
-
     expect(errorWrap.childElementCount).toBe(0)
 
     const submitButton = getByTestId('submit') as HTMLButtonElement
-
     expect(submitButton.disabled).toBe(true)
+
+    const emailStatus = getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo obrigatorio')
+    expect(emailStatus.textContent).toBe('🔴')
+
+    const passwordStatus = getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Campo obrigatorio')
+    expect(emailStatus.textContent).toBe('🔴')
   })
 })
