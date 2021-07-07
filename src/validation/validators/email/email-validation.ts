715318ed2eec8564c6ac28df6d323bc564eed0cd
vinisaveg <viniisaveeg@gmail.com>
2021-07-07 18:02:18 -0300

    feat: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -1,10 +1,11 @@
 import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
 import { FieldValidation } from '@/validation/protocols/field-validation'
-
 export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) {}
 
   validate(value: string): Error {
-    return new InvalidFieldError('email')
+    const emailRegex =
+      /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i
+    return emailRegex.test(value) ? null : new InvalidFieldError('email')
   }
 }
