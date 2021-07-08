1bf5c4deb6a42c1632758146ee7f7687ae6ee5eb
vinisaveg <viniisaveeg@gmail.com>
2021-07-08 14:31:26 -0300

    fix: ensure EmailValidation allows empty value

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -21,4 +21,12 @@ describe('EmailValidation', () => {
 
     expect(error).toBeFalsy()
   })
+
+  test('Should return falsy if email is empty', () => {
+    const sut = makeSut()
+
+    const error = sut.validate('')
+
+    expect(error).toBeFalsy()
+  })
 })
--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -6,6 +6,6 @@ export class EmailValidation implements FieldValidation {
   validate(value: string): Error {
     const emailRegex =
       /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i
-    return emailRegex.test(value) ? null : new InvalidFieldError('email')
+    return !value || emailRegex.test(value) ? null : new InvalidFieldError('email')
   }
 }
