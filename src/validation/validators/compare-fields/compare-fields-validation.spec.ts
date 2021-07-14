04bc9ced8a22e59ae4a98f87ed5586277a5bf415
vinisaveg <viniisaveeg@gmail.com>
2021-07-14 16:41:35 -0300

    test: ensure CompareFieldsValidation returns falsy if validation succeeds

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -9,9 +9,20 @@ const makeSut = (valueToCompare: string, field: string): CompareFieldsValidation
 describe('CompareFieldsValidation', () => {
   test('Should return error if compare is invalid', () => {
     const field = faker.database.column()
+
     const sut = makeSut(faker.random.word(), field)
     const error = sut.validate('')
 
     expect(error).toEqual(new InvalidFieldError(field))
+  })
+
+  test('Should return falsy if compare is valid', () => {
+    const field = faker.database.column()
+    const valueToCompare = faker.random.word()
+
+    const sut = makeSut(valueToCompare, field)
+    const error = sut.validate(valueToCompare)
+
+    expect(error).toBeFalsy()
   })
 })
