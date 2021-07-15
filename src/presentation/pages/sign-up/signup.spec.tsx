8a2d3d50083713cc1d647c77d6f02de22fa53431
vinisaveg <viniisaveeg@gmail.com>
2021-07-15 16:17:06 -0300

    test: ensure SignUp prevent submit if form is invalid

--- src/presentation/pages/sign-up/signup.spec.tsx
+++ src/presentation/pages/sign-up/signup.spec.tsx
@@ -159,4 +159,13 @@ describe('Signup component', () => {
     await simulateValidSubmit(sut)
     expect(addAccountSpy.callsCount).toBe(1)
   })
+
+  test('Should not call AddAccount if form is invalid', async () => {
+    const validationError = faker.random.words()
+
+    const { sut, addAccountSpy } = makeSut({ validationError })
+
+    await simulateValidSubmit(sut)
+    expect(addAccountSpy.callsCount).toBe(0)
+  })
 })
