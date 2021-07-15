88b8f7f63547546e4b25135d9b80836675aa859e
vinisaveg <viniisaveeg@gmail.com>
2021-07-15 15:59:25 -0300

    test: ensure SignUp prevent AddAccount to be called multiple times

--- src/presentation/pages/sign-up/signup.spec.tsx
+++ src/presentation/pages/sign-up/signup.spec.tsx
@@ -151,4 +151,12 @@ describe('Signup component', () => {
     await simulateValidSubmit(sut, name, email, password)
     expect(addAccountSpy.params).toEqual({ name, email, password, passwordConfirmation: password })
   })
+
+  test('Should call AddAccount only once', async () => {
+    const { sut, addAccountSpy } = makeSut()
+
+    await simulateValidSubmit(sut)
+    await simulateValidSubmit(sut)
+    expect(addAccountSpy.callsCount).toBe(1)
+  })
 })
