c812b347aad406b6dd2ddbf87b97aeece0d03fed
savegnago <savegdesigner@gmail.com>
2021-07-21 19:14:12 -0300

    test: ensure Login will not submit form if it is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -133,4 +133,14 @@ describe('Login', () => {
 
     cy.get('@request.all').should('have.length', 1)
   })
+
+  it('Should not call submit if form is invalid', () => {
+    cy.intercept('POST', /login/, {
+      statusCode: 200
+    }).as('request')
+
+    cy.getByTestId('email').focus().type(faker.internet.email()).type('{enter}')
+
+    cy.get('@request.all').should('have.length', 0)
+  })
 })
