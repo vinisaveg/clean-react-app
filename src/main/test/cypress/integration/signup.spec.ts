df89528f9f08d8acc2a074950d80dfe1207103d9
savegnago <savegdesigner@gmail.com>
2021-07-22 18:35:39 -0300

    test: ensure Signup loads with correct state

---
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -0,0 +1,24 @@
+describe('Signup', () => {
+  beforeEach(() => {
+    cy.visit('signup')
+  })
+
+  it('Should load with correct initial state', () => {
+    cy.getByTestId('name').should('have.attr', 'readOnly')
+    cy.getByTestId('name-status').should('have.attr', 'title', 'Campo obrigatorio').should('contain.text', '🔴')
+
+    cy.getByTestId('email').should('have.attr', 'readOnly')
+    cy.getByTestId('email-status').should('have.attr', 'title', 'Campo obrigatorio').should('contain.text', '🔴')
+
+    cy.getByTestId('password').should('have.attr', 'readOnly')
+    cy.getByTestId('password-status').should('have.attr', 'title', 'Campo obrigatorio').should('contain.text', '🔴')
+
+    cy.getByTestId('passwordConfirmation').should('have.attr', 'readOnly')
+    cy.getByTestId('passwordConfirmation-status')
+      .should('have.attr', 'title', 'Campo obrigatorio')
+      .should('contain.text', '🔴')
+
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('errorWrap').should('not.have.descendants')
+  })
+})
