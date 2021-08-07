16eccbd4a1687c536dfcbeb11ea7b635f2a3e72d
savegnago <savegdesigner@gmail.com>
2021-08-07 14:28:51 -0300

    refactor: move icon to its own file

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from 'react'
 
-import { Footer } from '@/presentation/components'
-import Header from '@/presentation/components/header/header'
+import { Footer, Icon } from '@/presentation/components'
+import { Header, IconName } from '@/presentation/components/'
 import Styles from './survey-list-styles.scss'
 
 const SurveyList: FunctionComponent = () => {
@@ -14,12 +14,7 @@ const SurveyList: FunctionComponent = () => {
         <ul>
           <li>
             <div className={Styles.surveyContent}>
-              <div className={[Styles.iconWrap, Styles.green].join(' ')}>
-                <img
-                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA0klEQVQ4EWNgIAH8//+/AYhLSNCCWynUMCD1/zcQG+BWSYQMkmEgA0Egjght2JUANYO8iQ4MsasmIAo0BZthP4DirAS0YkrjMAzk0tOYqgmIADUVgnTiADPxakfStAWmECj2DkmcWOYjoEJPRpBqmEGMQABiI4vB5IikH1PbQAYmIm0mVtlLahu4nJpe/gf0hho1XbgVGKd3qWngRFBA4/LyX6AcKZZdBbpOB2QgLk1nQJIkgElwtaBEDAXIOUULKHYSiP/CJHHQX4Hic4CYBWYgADx8PyqFiuhJAAAAAElFTkSuQmCC"
-                  alt="Thumb"
-                />
-              </div>
+              <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
               <time>
                 <span className={Styles.day}>08</span>
                 <span className={Styles.month}>07</span>
@@ -32,12 +27,7 @@ const SurveyList: FunctionComponent = () => {
 
           <li>
             <div className={Styles.surveyContent}>
-              <div className={[Styles.iconWrap, Styles.red].join(' ')}>
-                <img
-                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA70lEQVQ4Ea2RPQoCQQyFZ/w5g72lYOEVPIiV2IkIHmCvIZ5D77BgZWtrYWe1ICiuL8tEwjIZZmYNZCf7knyTzRrjrK7rAfwAr+AheyNZwiei98gNrBkISxYjz5KbZb0V4gXxlN8jzo+1tk91BOT6nhPmOFNg1Nb0UiCNxY0Uu8QW044BuMIZHs3DJzcra3/yOgem3UoT3pEcaQUh3TchAX9/KNTsy/mAtLebrzhXI+AqE/oQl55ErIfYxp5WothW71QyAJ0VWKG06DJAQ/jTA0yH0TUAzf4Gc8BFC5g3GcHI3IQvBy0asesDsB08CfYFB/44kX6+Hj8AAAAASUVORK5CYII="
-                  alt="Thumb"
-                />
-              </div>
+              <Icon className={Styles.iconWrap} iconName={IconName.thumbDown} />
               <time>
                 <span className={Styles.day}>08</span>
                 <span className={Styles.month}>07</span>
