e05c44826715d63fc9e4c1a284f77dc93fe2595e
savegnago <savegdesigner@gmail.com>
2021-08-07 14:45:18 -0300

    refactor: adapt layout to mobile

--- src/presentation/components/login-header/login-header.tsx
+++ src/presentation/components/login-header/login-header.tsx
@@ -6,7 +6,7 @@ import Logo from '../logo/logo'
 
 const LoginHeader: FunctionComponent = () => {
   return (
-    <header className={Styles.header}>
+    <header className={Styles.headerWrap}>
       <Logo />
       <h1>4Dev - Enquetes para programadores</h1>
     </header>
--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -37,6 +37,32 @@ const SurveyList: FunctionComponent = () => {
             </div>
             <footer>Ver resultado</footer>
           </li>
+
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
+              <time>
+                <span className={Styles.day}>08</span>
+                <span className={Styles.month}>07</span>
+                <span className={Styles.year}>2021</span>
+              </time>
+              <p>Qual é seu framework web favorito? </p>
+            </div>
+            <footer>Ver resultado</footer>
+          </li>
+
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
+              <time>
+                <span className={Styles.day}>08</span>
+                <span className={Styles.month}>07</span>
+                <span className={Styles.year}>2021</span>
+              </time>
+              <p>Qual é seu framework web favorito?</p>
+            </div>
+            <footer>Ver resultado</footer>
+          </li>
         </ul>
       </div>
       <Footer />
